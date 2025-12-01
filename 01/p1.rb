39bf83d32061c1a0f01623af06d4337b73d5d30c
ade ishs <adei@logicly.com.au>
2025-12-01 17:34:11 +1100

    rubocop

--- 01/p1.rb
+++ 01/p1.rb
@@ -1,8 +1,9 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 pos = 50
 pass = $stdin.each_line.map do |i|
-      pos += 100 + (i[0] == 'R' ? 1 : -1) * i[1...-1].to_i
-      pos % 100
-    end.select { |p| p.zero? }.size
+  pos += 100 + (i[0] == 'R' ? 1 : -1) * i[1...-1].to_i
+  pos % 100
+end.select(&:zero?).size
 puts pass
--- 01/p2.rb
+++ 01/p2.rb
@@ -1,20 +1,20 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 pos = 50
 pass = $stdin.each_line.map do |i|
-      s = i[0] == 'R' ? 1 : -1
-      m = i.chomp[1..-1].to_i
-      c = m / 100
-      r = m % 100
-      pre = pos
-      pos += s * r
+  s = i[0] == 'R' ? 1 : -1
+  m = i.chomp[1..].to_i
+  c = m / 100
+  pre = pos
+  pos += s * (m % 100)
 
-      if (pos % 100) == 0 ||
-          (!pre.zero? && (((pre + 100) / 100) != (pos + 100) / 100))
-        c += 1
-      end
+  if (pos % 100).zero? ||
+     (!pre.zero? && (((pre + 100) / 100) != (pos + 100) / 100))
+    c += 1
+  end
 
-      pos = (pos + 100) % 100
-      c
-    end.sum
+  pos = (pos + 100) % 100
+  c
+end.sum
 puts pass
