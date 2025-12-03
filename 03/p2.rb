dd8a874205b6193a69ebc903b9ac1f2e1d945e5f
ade ishs <adei@logicly.com.au>
2025-12-03 17:53:14 +1100

    Add day 3 part 2

---
+++ 03/p2.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+JOLTAGE_LEN = 12
+
+def find_joltage(batt)
+  rem = JOLTAGE_LEN
+  m = batt[0..-rem].each_char.max
+  i = batt.index(m) + 1
+  js = [m]
+  rem -= 1
+  while rem.positive?
+    m = batt[i..-rem].chars.max
+    i += batt[i..-rem].index(m) + 1
+    js << m
+    rem -= 1
+  end
+  js.join.to_i
+end
+
+sum = $stdin.each_line.reduce(0) { |a, b| a + find_joltage(b.chomp) }
+puts sum
