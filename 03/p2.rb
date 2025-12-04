a1bba0cd73b5e2a5bed74cc71e7e008f148289e9
ade ishs <adei@logicly.com.au>
2025-12-04 13:58:01 +1100

    Shorten code

--- 03/p2.rb
+++ 03/p2.rb
@@ -4,16 +4,12 @@
 JOLTAGE_LEN = 12
 
 def find_joltage(batt)
-  rem = JOLTAGE_LEN
-  js = []
   i = 0
-  while rem.positive?
+  (JOLTAGE_LEN..1).step(-1).reduce('') do |a, rem|
     m = batt[i..-rem].chars.max
     i += batt[i..-rem].index(m) + 1
-    js << m
-    rem -= 1
-  end
-  js.join.to_i
+    a + m
+  end.to_i
 end
 
 sum = $stdin.each_line.reduce(0) { |a, b| a + find_joltage(b.chomp) }
