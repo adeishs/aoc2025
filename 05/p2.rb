f0c6c188d8f67c13be033cd4b151f01098e52372
ade ishs <ade@adeishs.com>
2025-12-05 22:12:16 +1100

    Make the code look more FP

--- 05/p2.rb
+++ 05/p2.rb
@@ -10,17 +10,14 @@ def parse(lines)
        .map { |l| Range.new(*l.split('-').map(&:to_i)) }
 end
 
-fresh_ranges = parse($stdin.read).sort do |a, b|
-  a.begin == b.begin ? a.end <=> b.end : a.begin <=> b.begin
-end
-
-ranges = [fresh_ranges.shift]
-fresh_ranges.each do |r|
-  ranges << if ranges.last.cover?(r.begin)
-              l = ranges.pop
-              Range.new(l.begin, [r.end, l.end].max)
-            else
-              r
-            end
-end
+ranges =
+  parse($stdin.read).sort { |a, b| a.begin <=> b.begin || a.end <=> b.end }
+                    .each_with_object([]) do |curr, rs|
+                      rs << if !rs.empty? && rs.last.cover?(curr.begin)
+                              l = rs.pop
+                              Range.new(l.begin, [curr.end, l.end].max)
+                            else
+                              curr
+                            end
+                    end
 puts ranges.sum(&:size)
