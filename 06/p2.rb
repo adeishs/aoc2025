138bdbab66305907244bd0ab026ac65a8f2c9e66
ade ishs <ade@adeishs.com>
2025-12-07 15:59:53 +1100

    Make it look more FP

--- 06/p2.rb
+++ 06/p2.rb
@@ -6,9 +6,9 @@ operators = rows.pop
                 .chars
                 .map.with_index { |c, i| { op: c, pos: i } }
                 .reject { |o| o[:op] == ' ' }
-operators << { pos: operators.last[:pos] + 4 }
-sum = 0
-operators.each_cons(2) do |ops|
+results = (
+  operators + [{ pos: operators.last[:pos] + 4 }]
+).each_cons(2).each_with_object([]) do |ops, result|
   vals = (
     ops.first[:pos]...ops.last[:pos] - 1
   ).each_with_object([]) { |i, os| os << rows.map { |r| r[i] }.join.to_i }
@@ -17,6 +17,6 @@ operators.each_cons(2) do |ops|
     operands = vals.pop(2)
     vals << operands.first.send(ops.first[:op], operands.last)
   end
-  sum += vals.first
+  result << vals.first
 end
-puts sum
+puts results.sum
