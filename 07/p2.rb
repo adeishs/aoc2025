6b937a202873c542621fdfb080b939e40a9f2c5e
ade ishs <ade@adeishs.com>
2025-12-07 18:37:38 +1100

    Add day 7 part 2

---
+++ 07/p2.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'set'
+
+coords = $stdin.readlines
+               .map.with_index do |row, y|
+                 row.chars.map.with_index do |c, x|
+                   c == '^' ? Complex(x, y) : nil
+                 end
+               end.flatten.reject(&:nil?)
+splits = Hash.new(0)
+splits[coords.first] = 1
+
+(1...coords.size).each do |i|
+  curr = coords[i]
+  (i - 1..0).step(-1).each do |j|
+    prev = coords[j]
+    break if prev.real == curr.real
+
+    splits[curr] += splits[prev] if (prev.real - curr.real).abs == 1
+  end
+end
+
+puts splits.values.sum + 1
