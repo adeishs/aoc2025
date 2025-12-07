c77de64c6bc413de79e609aca72511fe6049ea73
ade ishs <ade@adeishs.com>
2025-12-07 23:07:32 +1100

    Refactor

--- 07/p2.rb
+++ 07/p2.rb
@@ -10,13 +10,15 @@ coords = $stdin.readlines
 splits = Hash.new(0)
 splits[coords.first] = 1
 
-(1...coords.size).each do |i|
-  curr = coords[i]
-  (i - 1..0).step(-1).each do |j|
-    prev = coords[j]
+coords[1...coords.size].each.with_index do |curr, i|
+  coords[
+    0..i
+  ].reject { |prev| prev.imag == curr.imag || (prev.real - curr.real).abs > 1 }
+    .reverse
+    .each do |prev|
     break if prev.real == curr.real
 
-    splits[curr] += splits[prev] if (prev.real - curr.real).abs == 1
+    splits[curr] += splits[prev]
   end
 end
 
