494818030db6ad0a1f926de8d2750f0eca9e5a3b
ade ishs <ade@adeishs.com>
2025-12-09 23:48:21 +1100

    Shorten code

--- 09/p1.rb
+++ 09/p1.rb
@@ -1,13 +1,8 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-def get_area(a, b)
-  d = Complex(1, 1) + a - b
-  d.real * d.imag
-end
-
 puts $stdin.readlines
            .map { |l| Complex(*l.chomp.split(',').map(&:to_i)) }
            .combination(2)
-           .map { |a, b| get_area(a, b) }
+           .map { |a, b| (a - b).rect.reduce(1) { |m, c| m * (c.abs + 1) } }
            .max
