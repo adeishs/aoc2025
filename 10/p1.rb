065890302c5945e016064ba985b9c13453afdaab
ade ishs <adei@logicly.com.au>
2025-12-10 17:44:19 +1100

    Add day 10 part 1

---
+++ 10/p1.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+def parse(line)
+  els = line.split(' ')
+  light_str = els.shift
+  joltage_str = els.pop
+  {
+    len: light_str[1...-1].size,
+    light: light_str[1...-1].tr('.#', '01').reverse.to_i(2),
+    buttons: els.map do |b|
+      b[1...-1].split(',').reduce(0) { |a, s| a ^ (1 << s.to_i) }
+    end,
+    joltages: joltage_str[1...-1].split(',').map(&:to_i)
+  }
+end
+
+def solve(machine)
+  n = 1
+  while n <= machine[:len]
+    machine[:buttons].combination(n).each do |buttons|
+      return n if machine[:light] == buttons.reduce(0) { |a, b| a ^ b }
+    end
+    n += 1
+  end
+end
+
+puts $stdin.readlines.map { |l| solve(parse(l.chomp)) }.sum
